f80a2a79030e5d31e3378d77ed9ac724646d6499
andikadirmayuda <andikadirmayuda21@mhs.mdp.ac.id>
2024-06-10 01:11:44 +0700

    day03

---
+++ fromzero/resources/views/blog.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blog</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <a href="/">Home</a>
+    <a href="/about">about</a>
+    <a href="/blog">blog</a>
+    <a href="/contact">Contact</a>
+    <br>
+    <h1>BLOG</h1>
+    <h3>Hai</h3>
+    <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore, enim! Quibusdam repellendus reiciendis corporis eaque magni deleniti eveniet labore inventore natus veniam quasi itaque placeat nesciunt rerum, repellat minus neque deserunt cum quis quae laudantium cupiditate illo? Distinctio quasi dolores ducimus molestias veniam, ullam quam, eum doloremque quas dolorem consequatur voluptas pariatur reprehenderit facilis laudantium minus, qui consequuntur blanditiis neque iusto repudiandae autem cupiditate. Quod voluptatibus facere, recusandae deserunt molestiae sunt tenetur! Ullam doloribus, aspernatur minus error a eius excepturi minima consequatur nostrum ad rem saepe cumque itaque nam incidunt omnis molestias tempore. Maiores repellat, inventore nobis exercitationem praesentium delectus.
+    </p>
+    <h3>Yuhu</h3>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, aspernatur.
+    </p>
+</body>
+</html>
---
+++ fromzero/resources/views/contact.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contact</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <a href="/">Home</a>
+    <a href="/about">about</a>
+    <a href="/blog">blog</a>
+    <a href="/contact">Contact</a>
+    <br>
+    <h1>Contact</h1>
+    
+    <tr>
+        <td>
+            Name = axc
+        </td>
+        <td>+62xxx</td>
+    </tr>
+</body>
+</html>
